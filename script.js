91202ad837c1d5cfc0a41f1e58382883f5916652
Awesome65432139 <94124691+Awesome65432139@users.noreply.github.com>
2024-01-11 15:19:34 +0000

    fixed js for swup

--- script.js
+++ script.js
@@ -1,108 +1,231 @@
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+document.addEventListener('DOMContentLoaded', () => {
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  
+  let interval = null;
+  
+  var card1 = 0;
+  var card2 = 0;
+  var card3 = 0;
+  var card4 = 0;
+  
+  document.querySelector("#effect1").onmouseover = (event) => {
+    let iteration = 0;
+  
+    clearInterval(interval);
+  
+    interval = setInterval(() => {
+      event.target.innerText = event.target.innerText
+        .split("")
+        .map((letter, index) => {
+          if (index < iteration) {
+            return event.target.dataset.value[index];
+          }
+  
+          return letters[Math.floor(Math.random() * 26)];
+        })
+        .join("");
+  
+      if (iteration >= event.target.dataset.value.length) {
+        clearInterval(interval);
+      }
+  
+      iteration += 1 / 3;
+    }, 20);
+  };
+  
+  document.querySelector("#effect2").onmouseover = (event) => {
+    let iteration = 0;
+  
+    clearInterval(interval);
+  
+    interval = setInterval(() => {
+      event.target.innerText = event.target.innerText
+        .split("")
+        .map((letter, index) => {
+          if (index < iteration) {
+            return event.target.dataset.value[index];
+          }
+  
+          return letters[Math.floor(Math.random() * 26)];
+        })
+        .join("");
+  
+      if (iteration >= event.target.dataset.value.length) {
+        clearInterval(interval);
+      }
+  
+      iteration += 1 / 3;
+    }, 20);
+  };
+  
+  document.querySelector("#effect3").onmouseover = (event) => {
+    let iteration = 0;
+  
+    clearInterval(interval);
+  
+    interval = setInterval(() => {
+      event.target.innerText = event.target.innerText
+        .split("")
+        .map((letter, index) => {
+          if (index < iteration) {
+            return event.target.dataset.value[index];
+          }
+  
+          return letters[Math.floor(Math.random() * 26)];
+        })
+        .join("");
+  
+      if (iteration >= event.target.dataset.value.length) {
+        clearInterval(interval);
+      }
+  
+      iteration += 1 / 3;
+    }, 20);
+  };
+  
+  document.querySelector("#effect4").onmouseover = (event) => {
+    let iteration = 0;
+  
+    clearInterval(interval);
+  
+    interval = setInterval(() => {
+      event.target.innerText = event.target.innerText
+        .split("")
+        .map((letter, index) => {
+          if (index < iteration) {
+            return event.target.dataset.value[index];
+          }
+  
+          return letters[Math.floor(Math.random() * 26)];
+        })
+        .join("");
+  
+      if (iteration >= event.target.dataset.value.length) {
+        clearInterval(interval);
+      }
+  
+      iteration += 1 / 3;
+    }, 20);
+  };
+  
+  document.getElementById("effect1").innerHTML = "Hover Here!‎‎‎‎";
+  document.getElementById("effect2").innerHTML = "‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎";
+  document.getElementById("effect3").innerHTML = "‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎";
+  document.getElementById("effect4").innerHTML = "‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎";}
+);
 
-let interval = null;
-
-var card1 = 0;
-var card2 = 0;
-var card3 = 0;
-var card4 = 0;
-
-document.querySelector("#effect1").onmouseover = (event) => {
-  let iteration = 0;
-
-  clearInterval(interval);
-
-  interval = setInterval(() => {
-    event.target.innerText = event.target.innerText
-      .split("")
-      .map((letter, index) => {
-        if (index < iteration) {
-          return event.target.dataset.value[index];
-        }
-
-        return letters[Math.floor(Math.random() * 26)];
-      })
-      .join("");
-
-    if (iteration >= event.target.dataset.value.length) {
-      clearInterval(interval);
-    }
-
-    iteration += 1 / 3;
-  }, 20);
-};
-
-document.querySelector("#effect2").onmouseover = (event) => {
-  let iteration = 0;
-
-  clearInterval(interval);
-
-  interval = setInterval(() => {
-    event.target.innerText = event.target.innerText
-      .split("")
-      .map((letter, index) => {
-        if (index < iteration) {
-          return event.target.dataset.value[index];
-        }
-
-        return letters[Math.floor(Math.random() * 26)];
-      })
-      .join("");
-
-    if (iteration >= event.target.dataset.value.length) {
-      clearInterval(interval);
-    }
-
-    iteration += 1 / 3;
-  }, 20);
-};
-
-document.querySelector("#effect3").onmouseover = (event) => {
-  let iteration = 0;
-
-  clearInterval(interval);
-
-  interval = setInterval(() => {
-    event.target.innerText = event.target.innerText
-      .split("")
-      .map((letter, index) => {
-        if (index < iteration) {
-          return event.target.dataset.value[index];
-        }
-
-        return letters[Math.floor(Math.random() * 26)];
-      })
-      .join("");
-
-    if (iteration >= event.target.dataset.value.length) {
-      clearInterval(interval);
-    }
-
-    iteration += 1 / 3;
-  }, 20);
-};
-
-document.querySelector("#effect4").onmouseover = (event) => {
-  let iteration = 0;
-
-  clearInterval(interval);
-
-  interval = setInterval(() => {
-    event.target.innerText = event.target.innerText
-      .split("")
-      .map((letter, index) => {
-        if (index < iteration) {
-          return event.target.dataset.value[index];
-        }
-
-        return letters[Math.floor(Math.random() * 26)];
-      })
-      .join("");
-
-    if (iteration >= event.target.dataset.value.length) {
-      clearInterval(interval);
-    }
-
-    iteration += 1 / 3;
-  }, 20);
-};
+swup.hooks.on('page:view', () => {
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  
+  let interval = null;
+  
+  var card1 = 0;
+  var card2 = 0;
+  var card3 = 0;
+  var card4 = 0;
+  
+  document.querySelector("#effect1").onmouseover = (event) => {
+    let iteration = 0;
+  
+    clearInterval(interval);
+  
+    interval = setInterval(() => {
+      event.target.innerText = event.target.innerText
+        .split("")
+        .map((letter, index) => {
+          if (index < iteration) {
+            return event.target.dataset.value[index];
+          }
+  
+          return letters[Math.floor(Math.random() * 26)];
+        })
+        .join("");
+  
+      if (iteration >= event.target.dataset.value.length) {
+        clearInterval(interval);
+      }
+  
+      iteration += 1 / 3;
+    }, 20);
+  };
+  
+  document.querySelector("#effect2").onmouseover = (event) => {
+    let iteration = 0;
+  
+    clearInterval(interval);
+  
+    interval = setInterval(() => {
+      event.target.innerText = event.target.innerText
+        .split("")
+        .map((letter, index) => {
+          if (index < iteration) {
+            return event.target.dataset.value[index];
+          }
+  
+          return letters[Math.floor(Math.random() * 26)];
+        })
+        .join("");
+  
+      if (iteration >= event.target.dataset.value.length) {
+        clearInterval(interval);
+      }
+  
+      iteration += 1 / 3;
+    }, 20);
+  };
+  
+  document.querySelector("#effect3").onmouseover = (event) => {
+    let iteration = 0;
+  
+    clearInterval(interval);
+  
+    interval = setInterval(() => {
+      event.target.innerText = event.target.innerText
+        .split("")
+        .map((letter, index) => {
+          if (index < iteration) {
+            return event.target.dataset.value[index];
+          }
+  
+          return letters[Math.floor(Math.random() * 26)];
+        })
+        .join("");
+  
+      if (iteration >= event.target.dataset.value.length) {
+        clearInterval(interval);
+      }
+  
+      iteration += 1 / 3;
+    }, 20);
+  };
+  
+  document.querySelector("#effect4").onmouseover = (event) => {
+    let iteration = 0;
+  
+    clearInterval(interval);
+  
+    interval = setInterval(() => {
+      event.target.innerText = event.target.innerText
+        .split("")
+        .map((letter, index) => {
+          if (index < iteration) {
+            return event.target.dataset.value[index];
+          }
+  
+          return letters[Math.floor(Math.random() * 26)];
+        })
+        .join("");
+  
+      if (iteration >= event.target.dataset.value.length) {
+        clearInterval(interval);
+      }
+  
+      iteration += 1 / 3;
+    }, 20);
+  };
+  
+  document.getElementById("effect1").innerHTML = "Hover Here!‎‎‎‎";
+  document.getElementById("effect2").innerHTML = "‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎";
+  document.getElementById("effect3").innerHTML = "‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎";
+  document.getElementById("effect4").innerHTML = "‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎‎";
+});
